ade6bdcac49dcf77f80d96a3256e8ac6d5c167ed
mogana varshini <92911280+Mogana004@users.noreply.github.com>
2024-03-10 14:04:20 +0530

    Create script.js

---
+++ TimeBOOM /script.js
@@ -0,0 +1,21 @@
+let timerEl = document.getElementById("timer");
+let defuserEl = document.getElementById("defuser");
+let countdown = 10;
+let intervalId = setInterval(function() {
+    countdown = countdown - 1;
+    timerEl.textContent = countdown;
+    if (countdown === 0) {
+        timerEl.textContent = "BOOM";
+        clearInterval(intervalId);
+    }
+
+
+}, 1000);
+
+defuserEl.addEventListener("keydown", function(event) {
+    let bombDefuserText = defuserEl.value;
+    if (event.key == "Enter" && bombDefuserText === "defuse" && countdown !== 0) {
+        timerEl.textContent = "You did it";
+        clearInterval(intervalId);
+    };
+});
